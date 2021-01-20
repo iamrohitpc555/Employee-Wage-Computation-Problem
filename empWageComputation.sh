1029aeb17b19f1de2f3683e9e8d5f8d9fe04406a
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-01-20 22:40:10 +0530

    Update empWageComputation.sh
    Added : Calculate Wages For a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,48 +2,57 @@
 
 echo "Welcome to Employee Wage Computation"
 
+#check employee is present or absent
+
 #constant values
-EMP_RATE_PER_HOUR=20
-NUMBER_OF_WORKING_DAYS=20
-MAX_HRS_IN_MONTH=100
-
-#variables
+empRatePerHr=20
+empFullTimeHrs=8
+empPartTimeHrs=4
+numberOfWorkingDays=20
 totalSalary=0
-totalEmpHr=0
-totalWorkingDays=0
 
-function getWorkingHours() {
-   empCheck=$(( RANDOM%3 + 1 ))
-     case $empCheck in
-      1)
-       empHrs=8
-       ;;
-      2)
-       empHrs=4
-       ;;
-      3)
-       empHrs=0
-       ;;
-     esac
-    echo $empHrs
-}
+#it will give 0 or 1 by using random
+attendance_check=$(( RANDOM%2 ))
 
-function calculateDailyWage() {
-     local workHours=$1
-     wages=$(( $workHours * $EMP_RATE_PER_HOUR ))
-     echo $wages
-}
+#check the condition if it gives 1 then true
+if [[ $attendance_check -eq 1 ]]
+then
+	echo employee is present
+	salary=$(( $empRatePerHr * $empFullTimeHrs ))
+	halfSalary=$(( $empRatePerHr * $empPartTimeHrs ))
+else
+	echo employee is absent
+	salary=0
+	halfSalary=0
+fi
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUMBER_OF_WORKING_DAYS ]]
+#employee wage using case statement
+#calculating wages for a month
+
+for (( i=1; i<=numberOfWorkingDays; i++ ))
 do
-   (( totalWorkingDays++ ))
-   workingHours="$( getWorkingHours )"
-   totalWorkHrs=$(( $totalEmpHr + $workingHours ))
-   empDailyWage["$totalWorkingDays"]="$( calculateDailyWage $workingHours )"
-done
-   echo $totalWorkHrs
+	empCheck=$(( RANDOM%3 + 1 ))
+	case $empCheck in
+		1)
+		empHrs=8
+		;;
+		2)
+		empHrs=4
+		;;
+		3)
+		empHrs=0
+		;;
+	esac
 
-   #calculate wages per month
-   totalSalary="$( calculateDailyWage $totalWorkHrs )"
-   echo "Daily wage ${empDailyWage[@]}"
-   echo "All keys  ${!empDailyWage[@]}"
+	#calculate employee wage
+	employeeWage=$(( $empHrs * $empRatePerHr ))
+
+	#calculate wages per month
+	totalSalary=$(( $totalSalary + $employeeWage ))
+
+done
+
+echo "salary of an employee:" $salary
+echo "part time salary of an employee:" $halfSalary
+echo "Employee Wage:" $employeeWage
+echo "Salary per month:" $totalSalary
