3343883da22431476cba059e1a115934b980a68f
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-01-20 22:42:33 +0530

    Update empWageComputation.sh
    Added : Store Daily Wage with Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,12 @@
 
 echo "Welcome to Employee Wage Computation"
 
+#check employee is present or absent
+
 #constant values
-EMP_RATE_PER_HOUR=20
-NUMBER_OF_WORKING_DAYS=20
-MAX_HRS_IN_MONTH=100
+empRatePerHr=20
+numberOfWorkingDays=20
+maxHrsInMonth=100
 
 #variables
 totalSalary=0
@@ -13,37 +15,35 @@ totalEmpHr=0
 totalWorkingDays=0
 
 function getWorkingHours() {
-   empCheck=$(( RANDOM%3 + 1 ))
-     case $empCheck in
-      1)
-       empHrs=8
-       ;;
-      2)
-       empHrs=4
-       ;;
-      3)
-       empHrs=0
-       ;;
-     esac
-    echo $empHrs
+	empCheck=$(( RANDOM%3 + 1 ))
+	case $empCheck in
+		1)
+		empHrs=8
+		;;
+		2)
+		empHrs=4
+		;;
+		3)
+		empHrs=0
+		;;
+	esac
+	echo $empHrs
 }
 
 function calculateDailyWage() {
-     local workHours=$1
-     wages=$(( $workHours * $EMP_RATE_PER_HOUR ))
-     echo $wages
+	local workHours=$1
+	wages=$(( $workHours * $empRatePerHr ))
+	echo $wages
 }
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUMBER_OF_WORKING_DAYS ]]
+while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numberOfWorkingDays ]]
 do
-   (( totalWorkingDays++ ))
-   workingHours="$( getWorkingHours )"
-   totalWorkHrs=$(( $totalEmpHr + $workingHours ))
-   empDailyWage["$totalWorkingDays"]="$( calculateDailyWage $workingHours )"
+	(( totalWorkingDays++ ))
+	workingHours="$( getWorkingHours )"
+	totalWorkHrs=$(( $totalEmpHr + $workingHours ))
+	empDailyWage[$totalWorkingDays]="$( calculateDailyWage $workingHours )"
 done
-   echo $totalWorkHrs
-
-   #calculate wages per month
-   totalSalary="$( calculateDailyWage $totalWorkHrs )"
-   echo "Daily wage ${empDailyWage[@]}"
-   echo "All keys  ${!empDailyWage[@]}"
+	echo $totalWorkHrs
+	#calculate wages per month
+	totalSalary="$( calculateDailyWage $totalWorkHrs )"
+	echo "Daily wage  ${empDailyWage[@]}"
